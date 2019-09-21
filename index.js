7cf334574e17ec01363345bae7a9b6c4c3661f83
于源广 <1035883599@qq.com>
2019-09-21 15:24:52 +0800

    更新多人聊天功能

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ var fs = require("fs");
 var globalConfig = require("./config");
 var loader = require("./loader");
 var log = require("./log");
+const WebSocket = require("ws"); // 引入 ws 库
+const wss = new WebSocket.Server({ port: 1230 }); // 声明wss对象
+wss.broadcastToElse = function broadcast(data, ws) {
+  wss.clients.forEach(function each(client) {
+    if (JSON.stringify(client) !== JSON.stringify(ws) && client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+/* 客户端接入，触发 connection */
+wss.on("connection", function connection(ws, req) {
+    console.log(1)
+  let ip = req.connection.remoteAddress; // 通过req对象可以获得客户端信息，比如：ip，headers等
+  /* 客户端发送消息，触发 message */
+  ws.on("message", function incoming(message) {
+    console.log(message)
+    ws.send(message); // 向客户端发送消息
+    wss.broadcastToElse(message, ws); // 向 其他的 客户端发送消息，实现群聊效果
+  });
+});
 
 http.createServer(function (request, response) {
 
